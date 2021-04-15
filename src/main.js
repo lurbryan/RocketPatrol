7c9a9d6773c9f9d81171a52a2c34bc17b990e244
lurbryan <lurbryan@ucsc.edu>
2021-04-14 17:13:32 -0700

    Reached "Go Live" step, still having problems with rocket motion.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-
-
 let config = {
     type: Phaser.Canvas,
     width: 640,
--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -2,10 +2,10 @@
 class Rocket extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
-
         scene.add.existing(this); // add to existing, displayList, updateList
         this.isFiring = false;    // track rocket's firing status
         this.moveSpeed = 2;       // pixels per frame
+        this.sfxRocket = scene.sound.add('sfx_rocket'); // add rocket sfx
     }
 
     update(){
@@ -19,8 +19,9 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF)){
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
+            this.sfxRocket.play();  // play sfx
         }
         // if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding){
@@ -28,8 +29,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
         // reset on miss
         if(this.y <= borderUISize * 3 + borderPadding){
-            this.isFiring = false;
-            this.y = game.config.height - borderUISize - borderPadding;
+            this.reset();
         }
+    }
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
